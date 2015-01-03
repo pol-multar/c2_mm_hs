82688d349778c6537fcbcdcae10f45d0f6a2dc78
Maxime MULTARI <maxime.multari@neuf.fr>
2015-01-03 15:29:33 +0100

    Adding comments in BoxPanel

--- src/main/java/fr/unice/polytech/si4/complexite/BoxPanel.java
+++ src/main/java/fr/unice/polytech/si4/complexite/BoxPanel.java
@@ -4,11 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 /**
- * Created by Hugo on 03/01/2015.
+ * The class to display a Box
+ * @author Hugo
+ * @version 03/01/2015.
  */
 public class BoxPanel extends JPanel {
     private final int squareSize = 30;
 
+    /**
+     * The constructor of a BoxPanel
+     * @param b the box to be displayed
+     */
     public BoxPanel(Box b) {
         setLayout(new GridLayout(b.getHeight(), b.getWidth()));
         for(int i=0; i<b.getHeight(); i++){
@@ -24,6 +30,13 @@ public class BoxPanel extends JPanel {
         setPreferredSize(new Dimension(squareSize * b.getWidth(), squareSize * b.getHeight()));
     }
 
+    /**
+     * A method to set the border
+     * @param p the panel to be modified
+     * @param b the box used with the panel
+     * @param x the row
+     * @param y the colomn
+     */
     public void setBorder(JPanel p, Box b, int x, int y){
         int id = b.getId(x, y);
         p.setBorder(BorderFactory.createMatteBorder(b.getId(x, y-1)!=id?1:0,
